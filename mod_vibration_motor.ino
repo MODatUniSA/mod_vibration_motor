0bc43c003dab6600bfd4e6184f39c23082994017
sighmon <simon.loffler@gmail.com>
2018-03-28 19:13:52 +1030

    Arduino code for an Adafruit Trinket M0 to control two mini vibration motors.

---
+++ mod_vibration_motor.ino
@@ -0,0 +1,41 @@
+// MOD. vibration motors
+// =====================
+
+// Parts
+// -----
+// Arduino micro-controller - Adafruit Trinket M0
+// Battery charger - Adafruit Powerboost 1000C
+// Vibration motors - 2mm 1020 Motors from Seeed
+// Transistor for switching on the motors - BD681
+// Protection diode for the motors - 1N4007
+// Resistor for the Arduino pin - 4.7 kohm
+
+// Set names for the vibration motor pins
+int motorPin1 = 0;
+int motorPin2 = 1;
+
+void setup() {
+  // Setup the vibration motor pins as output pins
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+}
+
+void loop() {
+  // Turn on each for a random time with a random delay
+  digitalWrite(motorPin1, HIGH);
+  delay(randomDelay());
+  digitalWrite(motorPin1, LOW);
+  delay(randomDelay());
+
+  digitalWrite(motorPin2, HIGH);
+  delay(randomDelay());
+  digitalWrite(motorPin2, LOW);
+  delay(randomDelay());
+}
+
+int randomDelay() {
+  // Delay between 200 ms and 1 second
+  return random(200, 1000);
+}
+
+
